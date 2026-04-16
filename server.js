fac90ec5105f8ea5a4a4bf9f368602e6480b2e9c
Evan Yang <evanjyang4@gmail.com>
2026-04-16 09:25:22 -0700

    '/api' prefix

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const apiRoutes = require('./routes');
+const apiRoutes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", apiRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB connected successfully.');
+    console.log("MongoDB connected successfully.");
 
     // Start the server only after the DB connection is successful
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error("MongoDB connection error:", err));
